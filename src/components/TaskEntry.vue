91cbc41ba0e648c3b4f54708084d83fdb66a2b64
凌莞 <i@gao4.pw>
2020-12-05 22:03:21 +0800

    use remote data

--- src/components/TaskEntry.vue
+++ src/components/TaskEntry.vue
@@ -5,6 +5,7 @@
             display: 'flex',
             alignItems: 'center',
             padding: '10px 20px',
+            minHeight: '40px',
         }"
     >
         <div class="bt" @click="setDone">
@@ -21,9 +22,32 @@
         </div>
         <router-link to="">
             <div class="body">
-                <span class="title" v-bind:class="{ del: done }">aaa</span>
-                <div class="detail">
-                    <span v-bind:class="{ del: done }">abc</span>
+                <span class="title" v-bind:class="{ del: done }">
+                    {{ name }}
+                </span>
+                <div
+                    class="detail"
+                    v-bind:class="{ missed, near, later, del: done }"
+                >
+                    <FontAwesomeIcon
+                        :icon="['fal', 'calendar-alt']"
+                        size="sm"
+                        v-if="later"
+                        v-bind:class="{ del: done }"
+                    ></FontAwesomeIcon>
+                    <FontAwesomeIcon
+                        :icon="['fal', 'calendar-times']"
+                        size="sm"
+                        v-if="missed"
+                        v-bind:class="{ del: done }"
+                    ></FontAwesomeIcon>
+                    <FontAwesomeIcon
+                        :icon="['fal', 'calendar-exclamation']"
+                        size="sm"
+                        v-if="near"
+                        v-bind:class="{ del: done }"
+                    ></FontAwesomeIcon>
+                    <span v-bind:class="{ del: done }">{{ duex }}</span>
                 </div>
             </div>
         </router-link>
@@ -50,11 +74,24 @@
         color: #303133;
     }
     .detail {
-        color: #606266;
         font-size: small;
+        align-items: center;
+        margin-left: 2px;
     }
-    .del{
+    .del {
         text-decoration: line-through;
+        color: #c0c4cc;
+    }
+    .detail span {
+        margin-left: 4px;
+    }
+    .missed {
+        color: #f56c6c;
+    }
+    .near {
+        color: #e6a23c;
+    }
+    .later {
         color: #909399;
     }
 </style>
@@ -64,17 +101,49 @@
     import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
     import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
     import { faCircle } from '@fortawesome/free-regular-svg-icons'
+    import { faCalendarAlt, faCalendarTimes, faCalendarExclamation } from '@fortawesome/pro-light-svg-icons'
 
-    library.add(faCheckCircle)
-    library.add(faCircle)
+    library.add(faCheckCircle, faCircle, faCalendarAlt, faCalendarTimes, faCalendarExclamation)
     export default {
         name: "TaskEntry",
         components: {
             FontAwesomeIcon
         },
+        data() {
+            return {
+                duex: 0,
+                near: false,
+                later: false,
+                missed: false
+            }
+        },
+        created() {
+            const now = new Date();
+            const year = now.getFullYear();
+            const month = now.getMonth() + 1;
+            const day = now.getDate();
+            const dateStart = new Date(`${year}-${month}-${day}`)
+            const dateEnd = new Date(this.due);
+            const difValue = (dateEnd - dateStart) / (1000 * 60 * 60 * 24);
+            if (difValue == 0) {
+                this.duex = 'Today'
+                this.near = true
+            }
+            else if (difValue == 1) {
+                this.duex = 'Tomorrow'
+                this.near = true
+            }
+            else if (difValue < 0) {
+                this.duex = this.due
+                this.missed = true
+            } else {
+                this.duex = this.due
+                this.later = true
+            }
+        },
         props: {
             name: String,
-            id: String,
+            key: String,
             due: String,
             done: Boolean
         },
--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -1,6 +1,20 @@
 <template>
-    <TaskEntry name="123" due="123"></TaskEntry>
+    <div>
+        <TaskEntry
+            v-for="task in sortTasks"
+            :key="task.id"
+            :name="task.name"
+            :due="task.due"
+            class="entry"
+        ></TaskEntry>
+    </div>
 </template>
+
+<style scoped>
+    .entry {
+        margin-bottom: 10px;
+    }
+</style>
 
 <script>
     import TaskEntry from '@/components/TaskEntry'
@@ -10,23 +24,37 @@
         },
         data() {
             return {
-                uid: window.uid,
-                token: window.token,
-                apiver: "",
+                tasks: []
             };
         },
+        computed: {
+            sortTasks() {
+                this.tasks.forEach(i => {
+                    if (i.status && i.status[window.uid]) {
+                        i.done = true
+                    }
+                });
+                this.tasks.sort(compareByDate)
+                return this.tasks
+            }
+        },
         created() {
-            // this.axios.get("/api/all")
-            //     .then((response) => {
-            //         this.apiver = response.data.version;
-            //     }).catch(error => {
-            //         if (error.response && error.response.status == 401) {
-            //             this.$router.push({ name: "unauthorized" });
-            //         }
-            //         else{
-            //             this.$router.push({ name: "servererr" });
-            //         }
-            //     })
+            this.axios.get("/api/all")
+                .then((response) => {
+                    this.tasks = response.data;
+                }).catch(error => {
+                    if (error.response && error.response.status == 401) {
+                        this.$router.push({ name: "unauthorized" });
+                    }
+                    else {
+                        this.$router.push({ name: "servererr" });
+                    }
+                })
         },
     };
+    function compareByDate(a, b) {
+        const dateStart = new Date(a)
+        const dateEnd = new Date(b)
+        return dateStart - dateEnd
+    }
 </script>
