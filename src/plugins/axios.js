438ca593d70b844f9cf341d254539e4cab0e1277
凌莞 <i@gao4.pw>
2020-12-06 14:09:39 +0800

    use environment

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -12,7 +12,7 @@ let config = {
   // baseURL: process.env.baseURL || process.env.apiUrl || ""
   // timeout: 60 * 1000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
-  baseURL: "https://api4.lwqwq.com",
+  baseURL: process.env.VUE_APP_BACKENDURL,
   timeout: 2000,
 };
 
